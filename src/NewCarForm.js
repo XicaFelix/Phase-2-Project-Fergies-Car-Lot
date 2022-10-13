1f57d05803edfa593679550673b17329da9b7d5f
Xica Felix <xicasfelix@gmail.com>
2022-10-13 18:41:38 -0400

    Wiring new car form to be controlled, adding state to record all user inputs

--- src/NewCarForm.js
+++ src/NewCarForm.js
@@ -1,35 +1,57 @@
 import { Button, Form} from "react-bootstrap";
+import { useState } from "react";
+
 
 function NewCarForm(){
-
+    const [newCar, setNewCar]=useState({
+        make : 's',
+        model: '',
+        year: '',
+        price: '',
+        frontImage: ' ',
+        sideImage: '',
+        backImage: '',
+    })
+    function handleChange(e){
+        const name = e.target.name;
+        let value = e.target.value;
+        setNewCar({...newCar, 
+        [name]: value,
+         })
+         console.log(newCar);
+    }
     return(
         <div>
-        <Form>
-        <Form.Group onChange={(e)=> console.log(e.target.value)}>
+        <Form onSubmit={(e)=> {e.preventDefault();console.log(newCar)}}>
+        <Form.Group onChange={handleChange}  value={newCar.make}>
             <Form.Label>Make:</Form.Label>
-            <Form.Control type="text" placeholder="Enter Make"/>
+            <Form.Control name="make" type="text" placeholder="Enter Make" />
         </Form.Group>
         <Form.Group>
             <Form.Label>Model:</Form.Label>
-            <Form.Control type="text" placeholder="Enter Model"/>
+            <Form.Control name='model' type="text" placeholder="Enter Model" onChange={handleChange} value={newCar.model}/>
         </Form.Group>
         <Form.Group>
             <Form.Label>Year:</Form.Label>
-            <Form.Control type="text" placeholder="Enter Year"/>
+            <Form.Control name='year' type="text" placeholder="Enter Year" onChange={handleChange} value={newCar.year}/>
         </Form.Group>
         <Form.Group>
             <Form.Label>Price:</Form.Label>
-            <Form.Control type="text" placeholder="Enter Price"/>
+            <Form.Control name='price' type="text" placeholder="Enter Price" onChange={handleChange} value={newCar.price}/>
+        </Form.Group>
+        <Form.Group onChange={handleChange}>
+            <Form.Label>Front Image:</Form.Label>
+            <Form.Control  name='frontImage' type="text" placeholder="Image URL" onChange={handleChange} value={newCar.frontImage}/>
         </Form.Group>
         <Form.Group>
-            <Form.Label>Front Image:</Form.Label>
-            <Form.Control type="text" placeholder="Image URL"/>
             <Form.Label>Side Image:</Form.Label>
-            <Form.Control type="text" placeholder="Image URL"/>
-            <Form.Label>Back Image:</Form.Label>
-            <Form.Control type="text" placeholder="Image URL"/>
+            <Form.Control  name='sideImage' type="text" placeholder="Image URL" onChange={handleChange}  value={newCar.sideImage}/>
         </Form.Group>
-        <Button type="submit" onSubmit={(e)=> {e.preventDefault();console.log(e.target.value)}}>Click to Submit</Button>
+        <Form.Group>
+            <Form.Label>Back Image:</Form.Label>
+            <Form.Control  name='backImage' type="text" placeholder="Image URL"onChange={handleChange} value={newCar.backImage}/>
+        </Form.Group>
+        <Button type="submit">Click to Submit</Button>
         </Form>
         </div>
     )
